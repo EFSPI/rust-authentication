7bede7b0a8f331a0e5012d9c11931bef2f37d148
Tanguy LH <tanguy.tlh@gmail.com>
2024-12-04 00:41:24 +0100

    Add -> refresh token

--- src/auth.rs
+++ src/auth.rs
@@ -1,10 +1,18 @@
 use jsonwebtoken::{encode, decode, Header, Validation, EncodingKey, DecodingKey, errors::Error};
 use serde::{Serialize, Deserialize};
 use std::time::{SystemTime, UNIX_EPOCH};
+use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
     pub sub: String,
+    pub exp: usize,
+    pub scope: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RefreshToken {
+    pub token: String,
     pub exp: usize,
 }
 
@@ -22,9 +30,15 @@ pub fn create_jwt(user_id: &str, secret: &str, expiration: usize) -> Result<Stri
     let claims = Claims {
         sub: user_id.to_owned(),
         exp,
+        scope: Some("read:write".to_string()),
     };
+
     encode(&Header::default(), &claims, &EncodingKey::from_secret(secret.as_ref()))
 
+}
+
+pub fn create_refresh_token() -> String {
+    Uuid::new_v4().to_string()
 }
 
 pub fn validate_jwt(token: &str, secret: &str) -> Result<Claims, Error> {
@@ -34,4 +48,39 @@ pub fn validate_jwt(token: &str, secret: &str) -> Result<Claims, Error> {
         &Validation::default(),
     )
     .map(|data| data.claims)
+}
+
+/// TODO: Réparer cette fonction pour utiliser une db, quitte à faire du sqlite3
+pub fn validate_refresh_token(refresh_token: &str) -> bool {
+    !refresh_token.is_empty()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // use jsonwebtoken::{encode, decode, Header, Validation, EncodingKey, DecodingKey };
+
+    const SECRET: &str = "super_secret_key";
+    const EXPTIME : usize = 3600;
+
+    #[test]
+    fn test_generate_jwt() {
+        let token = create_jwt("user123", SECRET, EXPTIME);
+        assert!(token.is_ok());
+    }
+
+    #[test]
+    fn test_validate_jwt() {
+        let token = create_jwt("user123", SECRET, EXPTIME)
+            .expect("Not buggy here");
+        let claims = validate_jwt(&token, SECRET).unwrap();
+        assert_eq!(claims.sub, "user123");
+    }
+
+    #[test]
+    fn test_validate_invalid_jwt() {
+        let invalid_token = "invalid_token_string";
+        let result = validate_jwt(invalid_token, SECRET);
+        assert!(result.is_err());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,11 @@
 mod auth;
+mod jwt_routes;
 
 use actix_web::{web, App, HttpServer, HttpResponse};
 use dotenv::dotenv;
 use std::env;
-use auth::{create_jwt, validate_jwt};
-
-async fn generate_token() -> HttpResponse {
-    let secret = env::var("SECRET_KEY").expect("SECRET_KEY must be set");
-
-    match create_jwt("user123", &secret, 3600) {
-        Ok(token) => HttpResponse::Ok().body(token),
-        Err(e) => {
-            eprintln!("Failed to create JWT: {}", e);
-            HttpResponse::InternalServerError().body("Failed to generate token")
-        }
-    }
-}
-
-async fn validate_token(path: web::Path<String>) -> HttpResponse {
-    let secret = env::var("SECRET_KEY").expect("SECRET_KEY must be set");
-    let token = path.into_inner();
-
-    println!("token {}", token);
-
-    match validate_jwt(&token, &secret) {
-        Ok(claims) => HttpResponse::Ok().body(format!("Valid token for user: {}", claims.sub)),
-        Err(e) => {
-            eprintln!("Invalid token: {}", e);
-            HttpResponse::Unauthorized().body("Invalid or expired token")
-        }
-    }
-}
-
+// use auth::{create_jwt, validate_jwt};
+use jwt_routes::{generate_token, validate_token};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
